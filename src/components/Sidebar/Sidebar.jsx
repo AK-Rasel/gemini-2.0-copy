c0da4162431b5b5b4ea21550f3cad13a56693078
AK-Rasel <raselak101010@gmail.com>
2024-10-30 20:20:29 +0600

    gemini 2.0 sidebar ui collapse functionality add

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,35 +1,45 @@
 import "./sidebar.css";
 import { assets } from "../../assets/assets.js";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
   return (
     <div className="sidebar">
       <div className="top">
-        <img className="menu" src={assets.menu_icon} alt="menu_icon" />
+        <img
+          onClick={() => setExtended((prev) => !prev)}
+          className="menu"
+          src={assets.menu_icon}
+          alt="menu_icon"
+        />
         <div className="new-chat">
           <img src={assets.plus_icon} alt="plus_icon" />
-          <p>New Chat</p>
+          {extended && <p>New Chat</p>}
         </div>
-        <div className="recent">
-          <p className="resent-title">Recent</p>
-          <div className="recent-entry">
-            <img src={assets.message_icon} alt="message_icon" />
-            <p>What is React ...</p>
+        {extended && (
+          <div className="recent">
+            <p className="resent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="message_icon" />
+              <p>What is React ...</p>
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="question_icon" />
-          <p>Help</p>
+          {extended && <p>Help</p>}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="history_icon" />
-          <p>Activity</p>
+          {extended && <p>Activity</p>}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="setting_icon" />
-          <p>Settings</p>
+
+          {extended && <p>Settings</p>}
         </div>
       </div>
     </div>
